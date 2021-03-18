e59ab86aefbb98f747ebfb6d8aa8501969857b9b
Gelller <geller.ggs@gmail.com>
2021-03-18 13:11:04 +0300

    Add project files.

---
+++ Microservice/ValuesHolder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microservice
+{
+    public class ValuesHolder
+    {
+        public List<string> Values { get; set; }
+        public ValuesHolder()
+        {
+            Values = new List<string>();
+        }
+    }
+}
